b23315ce41cefd5d621fb611dd1de62c6c5583e7
Zelda <zel.dasupakron@gmail.com>
2024-09-18 21:08:50 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-<<<<<<< HEAD
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+  compatibilityDate: "2024-09-18",
 })
-=======
-})
->>>>>>> 0774df1ac66625a491e8872dd96f8de85e64eea9
