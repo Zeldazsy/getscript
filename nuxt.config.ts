0774df1ac66625a491e8872dd96f8de85e64eea9
Zelda <166204162+Zeldazsy@users.noreply.github.com>
2024-09-18 21:01:02 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
 })
